3dcdf8c20932039c08f1978ade77d63108fd13cf
Shusetsu Toda <shusetsu@lightcurve.io>
2018-03-19 16:15:27 +0100

    :recycle: Changed namings

--- bin/lisky.js
+++ bin/lisky.js
@@ -17,74 +17,115 @@
 import os from 'os';
 import lockfile from 'lockfile';
 import semver from 'semver';
-// eslint-disable-next-line import/order
 import packageJSON from '../package.json';
 
 const nonInteractiveLiskyArg = process.argv[1];
 const nonInteractiveCommandArg = process.argv[2];
+const nonInteractiveOptions = process.argv.slice(3);
 
-process.env.LISKY_CONFIG_DIR =
-	process.env.LISKY_CONFIG_DIR || `${os.homedir()}/.lisky`;
-const configLockfilePath = `${process.env.LISKY_CONFIG_DIR}/config.lock`;
+const exit = code => process.exit(code || 0);
 
-process.env.NON_INTERACTIVE_MODE = !(
-	nonInteractiveLiskyArg.endsWith('lisky') && process.argv.length === 2
-);
+const printWarning = message => console.warn('\x1b[33m', message, '\x1b[0m');
+const printError = message => console.error('\x1b[31m', message, '\x1b[0m');
+const printVersion = version => console.info(version);
 
-function exit(code) {
-	process.exit(code || 0);
-}
-
-if (!semver.satisfies(process.version, packageJSON.engines.node)) {
-	console.error(
-		'\x1b[31m',
-		`ERROR: Requires Node.js version ${semver.clean(
-			packageJSON.engines.node,
-		)}, but was started with version ${semver.clean(process.version)}.`,
-		'\x1b[0m',
+const execClean = path => {
+	printWarning(
+		'WARNING: Attempting to remove configuration lockfile. I hope you know what you’re doing.',
 	);
-	exit();
-}
+	lockfile.unlockSync(path);
+};
 
-switch (process.argv[2]) {
-	case 'clean':
-		console.warn(
-			'\x1b[33m',
-			'WARNING: Attempting to remove configuration lockfile. I hope you know what you’re doing.',
-			'\x1b[0m',
+const isUnknownCommand = (liskyInstance, command) => {
+	const firstCommandWords = liskyInstance.commands.map(
+		// eslint-disable-next-line no-underscore-dangle
+		c => c._name.split(' ')[0],
+	);
+	return firstCommandWords.indexOf(command) === -1;
+};
+
+const setEnvironment = () => {
+	process.env.LISKY_CONFIG_DIR =
+		process.env.LISKY_CONFIG_DIR || `${os.homedir()}/.lisky`;
+
+	process.env.NON_INTERACTIVE_MODE = !(
+		nonInteractiveLiskyArg.endsWith('lisky') && process.argv.length === 2
+	);
+};
+
+const checkNodeVersion = (expected, actual) => {
+	if (!semver.satisfies(actual, expected)) {
+		throw new Error(
+			`ERROR: Requires Node.js version ${semver.clean(
+				expected,
+			)}, but was started with version ${semver.clean(actual)}.`,
 		);
-		lockfile.unlockSync(configLockfilePath);
-		exit();
-		break;
-	case '--version':
-	case '-v':
-		console.info(packageJSON.version);
-		exit();
-		break;
-	default:
-	// continue...
-}
-
-const lisky = require('../dist').default;
-const execFile = require('../dist/exec_file').default;
-
-// eslint-disable-next-line no-underscore-dangle
-const firstCommandWords = lisky.commands.map(c => c._name.split(' ')[0]);
-
-let commandArgIsFilePath = false;
-if (firstCommandWords.indexOf(nonInteractiveCommandArg) === -1) {
-	commandArgIsFilePath = true;
-	try {
-		const nonInteractiveOptions = process.argv.slice(3);
-		execFile(lisky, nonInteractiveCommandArg, nonInteractiveOptions, exit);
-	} catch (error) {
-		commandArgIsFilePath = false;
 	}
-}
+};
 
-if (!commandArgIsFilePath) {
-	module.exports =
-		process.env.NON_INTERACTIVE_MODE === 'true'
-			? lisky.parse(process.argv)
-			: lisky;
-}
+const handleBasicCommands = (command, lockFilePath, version) => {
+	switch (command) {
+		case 'clean':
+			execClean(lockFilePath);
+			return true;
+		case '--version':
+		case '-v':
+			printVersion(version);
+			return true;
+		default:
+			return false;
+	}
+};
+
+const getLiskyInstanceByMode = (liskyInstance, nonInteractiveMode) =>
+	nonInteractiveMode ? liskyInstance.parse(process.argv) : liskyInstance;
+
+const run = () => {
+	setEnvironment();
+	try {
+		checkNodeVersion(packageJSON.engines.node, process.version);
+	} catch (error) {
+		printError(error.message);
+		exit();
+	}
+
+	if (
+		handleBasicCommands(
+			nonInteractiveCommandArg,
+			`${process.env.LISKY_CONFIG_DIR}/config.lock`,
+			packageJSON.version,
+		)
+	) {
+		exit();
+	}
+
+	// Dynamically required, otherwise it starts the CLI before handling above codes
+	// eslint-disable-next-line global-require
+	const lisky = require('../dist').default;
+	// eslint-disable-next-line global-require
+	const execFile = require('../dist/exec_file').default;
+	if (isUnknownCommand(lisky, nonInteractiveCommandArg)) {
+		try {
+			// "execFile" throws error when it "nonInteractiveCommandArg" is not a filepath
+			return execFile(
+				lisky,
+				nonInteractiveCommandArg,
+				nonInteractiveOptions,
+				exit,
+			);
+		} catch (error) {
+			return getLiskyInstanceByMode(
+				lisky,
+				process.env.NON_INTERACTIVE_MODE === 'true',
+			);
+		}
+	}
+	return getLiskyInstanceByMode(
+		lisky,
+		process.env.NON_INTERACTIVE_MODE === 'true',
+	);
+};
+
+run();
+
+export default run;
